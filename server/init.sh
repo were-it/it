ba38a16951a66019a50738493448c560cd3b1c80
Josh Smith <josh@codecorps.org>
2023-04-27 12:25:14 -0400

    Fix init.sh

--- server/init.sh
+++ server/init.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+echo "$0 running as '$(whoami)'"
+
 RELEASE_NAME="${RELEASE_NAME:-"it-server"}"
 RELEASE_NODE="${RELEASE_NODE:-"$RELEASE_NAME"}"
 RELEASE_DISTRIBUTION="${RELEASE_DISTRIBUTION:-"sname"}"
